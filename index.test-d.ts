cb9e9ef9809f617543f005e9dd63ff7d4674e5b1
Sindre Sorhus <sindresorhus@gmail.com>
2019-05-30 16:32:58 +0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,8 +1,8 @@
 import {expectType} from 'tsd-check';
-import * as electronServe from '.';
-import { BrowserWindow } from 'electron';
+import {BrowserWindow} from 'electron';
+import serve = require('.');
 
-expectType<electronServe.loadURL>(electronServe({directory:""}));
+expectType<serve.loadURL>(serve({directory: ''}));
 
-const loadURL = electronServe({directory:""});
+const loadURL = serve({directory: ''});
 expectType<Promise<void>>(loadURL(new BrowserWindow()));
